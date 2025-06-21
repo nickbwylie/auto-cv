678d4e379bc67f73471ecb55d23a824cf1b2b9b3
Elmo0073! <nickbwylie@outlook.com>
2025-06-21 00:15:25 -0500

    fixed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import MapView from "./MapView";
 
 function App() {
   return (
-    <div className="min-h-screen w-full bg-gray-100">
+    <div className="w-screen h-full bg-slate-600">
+      {/* <h1>Hi</h1> */}
       <MapView />
     </div>
   );
